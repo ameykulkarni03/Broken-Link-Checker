f7eb85c385a912d873d81c8bea8b64f9980202a9
ameykulkarni03 <112329132+ameykulkarni03@users.noreply.github.com>
2023-09-20 20:50:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+function enableBtn() {
+    document.getElementById("checkButton").removeAttribute("disabled");
+}
+
 function checkLinks() {
     const urlInput = document.getElementById("url");
     const resultMessage = document.getElementById("result");
@@ -9,40 +13,15 @@ function checkLinks() {
         return;
     }
 
-    // Create a dummy <a> element to check links
-    const dummyLink = document.createElement("a");
-    dummyLink.href = url;
-
-    // Use XMLHttpRequest to fetch the HTML of the page
+    // Create an HTTP HEAD request to check links
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
+    xhr.open("HEAD", url, true);
     xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                const htmlContent = xhr.responseText;
-                const doc = new DOMParser().parseFromString(htmlContent, "text/html");
-                const links = doc.querySelectorAll("a");
-
-                let brokenLinks = 0;
-
-                // Check each link
-                links.forEach(function (link) {
-                    dummyLink.href = link.href;
-                    if (dummyLink.protocol === "http:" || dummyLink.protocol === "https:") {
-                        const linkStatus = dummyLink.status;
-                        if (linkStatus === 0 || (linkStatus >= 400 && linkStatus < 600)) {
-                            brokenLinks++;
-                        }
-                    }
-                });
-
-                if (brokenLinks === 0) {
-                    resultMessage.textContent = "No broken links found on this page.";
-                } else {
-                    resultMessage.textContent = `Found ${brokenLinks} broken links on this page.`;
-                }
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                resultMessage.textContent = "No broken links found on this page.";
             } else {
-                resultMessage.textContent = "Error: Unable to fetch the page.";
+                resultMessage.textContent = `Error: ${xhr.status} - Broken link detected.`;
             }
         }
     };
