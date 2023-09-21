ee7a17599d121fd437aa867a560350d72e91adf2
ameykulkarni03 <112329132+ameykulkarni03@users.noreply.github.com>
2023-09-21 10:37:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ function enableBtn() {
     document.getElementById("checkButton").removeAttribute("disabled");
 }
 
+
 function checkLinks() {
     const urlInput = document.getElementById("url");
     const resultMessage = document.getElementById("result");
@@ -13,17 +14,20 @@ function checkLinks() {
         return;
     }
 
-    // Create an HTTP HEAD request to check links
-    const xhr = new XMLHttpRequest();
-    xhr.open("HEAD", url, true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4) {
-            if (xhr.status == 200) {
+    // Construct the proxy URL with the user-specified URL as a query parameter
+    const proxyUrl = `proxy.php?url=${encodeURIComponent(url)}`;
+
+    // Make a request to the PHP proxy
+    fetch(proxyUrl)
+        .then(response => {
+            if (response.status === 200) {
                 resultMessage.textContent = "No broken links found on this page.";
             } else {
-                resultMessage.textContent = `Error: ${xhr.status} - Broken link detected.`;
+                resultMessage.textContent = `Error: ${response.status} - Broken link detected.`;
             }
-        }
-    };
-    xhr.send();
+        })
+        .catch(error => {
+            console.error(error);
+            resultMessage.textContent = "An error occurred while checking the link.";
+        });
 }
